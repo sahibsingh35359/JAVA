38902de1aebce51a59e2cb886e9cd62925dc72c8
sahibsingh35359 <85674126+sahibsingh35359@users.noreply.github.com>
2022-07-26 13:03:09 +0530

    Add files via upload

---
+++ for_loop.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class for_loop {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 13; i++) {
+            System.out.println(i);
+        }
+    }
+}
