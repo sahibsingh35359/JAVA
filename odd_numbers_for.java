27527802a4662472646e5872b60e2f0cbb1c6f13
sahibsingh35359 <85674126+sahibsingh35359@users.noreply.github.com>
2022-07-26 13:03:43 +0530

    Add files via upload

---
+++ odd_numbers_for.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class odd_numbers_for {
+    public static void main(String[] args) {
+        int n = 10;
+        for (int j = 0; j<=5; j++){
+            System.out.println(2*j+1);
+        }
+    }
+}
