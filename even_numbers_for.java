c1a00bdbb9ad1de0c87b9684546e8221c944aafd
sahibsingh35359 <85674126+sahibsingh35359@users.noreply.github.com>
2022-07-26 13:03:25 +0530

    Add files via upload

---
+++ even_numbers_for.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class even_numbers_for {
+    public static void main(String[] args) {
+        int n = 10;
+        for (int j = 0; j<=5; j++){
+            System.out.println(2*j);
+        }
+    }
+}
+
+
+
